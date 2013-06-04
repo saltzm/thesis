3067ab2f0e7fc3b19c36dec6cd9302bba019a17d
saltzm <saltzm@gmail.com>
2013-06-03 22:13:10 -0400

    new iso alg.

--- efficient_graph_sim/src/Main.scala
+++ efficient_graph_sim/src/Main.scala
@@ -14,14 +14,17 @@ object Main extends App {
 } 
 
 object RGDualSimTest extends App {
-  if (args.length != 3) println("Usage: <g_file> <q_file>")
+  if (args.length != 3) {
+    println("Usage: <g_file> <q_file>")
+    System.exit(-1)
+  }
   val g_file = args(0)
   val q_file = args(1)
   val sim_file = args(2)
-  val g = GraphGenerator.generateRandomGraph(1000000, 20, 16)
+  val g = GraphGenerator.generateRandomGraph(1000000, 200, 16)
   g.writeToFile(g_file)
   println("Done generating data graph")
-  val q = GraphGenerator.generateBFSQuery(4, 2, g)
+  val q = GraphGenerator.generateBFSQuery(25, 3, g)
   q.writeToFile(q_file)
   println("Done generating query graph")
   var t0 = System.nanoTime()
@@ -30,4 +33,32 @@ object RGDualSimTest extends App {
   PatternMatcher.writePhiToFile(sim, sim_file)
   println("Saltz Dual Sim took " + (t1 - t0)/1000000 + " ms")
   println("Found " + sim.flatten.toSet.size + " results.")
+}
+
+object SaltzIsoTest extends App {
+  val g_file = args(0)
+  val q_file = args(1)
+  var t0 = System.nanoTime()
+  val g = GraphGenerator.generateRandomGraph(10000, 20, 16)
+  g.writeToFile(g_file)
+  println("Done generating data graph")
+  val q = GraphGenerator.generateBFSQuery(4, 2, g)
+  q.writeToFile(q_file)
+  println("Done generating query graph")
+  var t1 = System.nanoTime()
+  val sim = PatternMatcher.saltzIso(g, q)
+  var t2 = System.nanoTime()
+  var i = 0
+ sim.foreach{ x => 
+    println("Result " + i) 
+    x.foreach{ y => println(y) }
+    println 
+    i += 1 
+  }
+  println("Graph creation time:       " + (t1 - t0)/1000000 + " ms")
+  println("Saltz Iso Took             " + (t2 - t1)/1000000 + " ms")
+  println("Total time:                " + (t2 - t0)/1000000 + " ms")
+  println("Found " + sim.size + " isomorphic matches.")
+  println("Found " + sim.flatten.toSet.size + " unique nodes.")
+ 
 }
--- efficient_graph_sim/src/PatternMatcher.scala
+++ efficient_graph_sim/src/PatternMatcher.scala
@@ -2,8 +2,32 @@ import java.io._
 
 object PatternMatcher { 
 
+  def saltzIso(g: Graph, q: Graph): Set[Array[Set[Int]]] = {
+    var initPhi = saltzDualSim(g, q, feasibleMates(g, q))
+    saltzIso(g, q, initPhi, 0) 
+  }
+
+  def saltzIso(g: Graph, q: Graph, phi: Array[Set[Int]], depth: Int): 
+               Set[Array[Set[Int]]] = {
+    if (depth == q.size) Set(phi)
+    else if (phi.isEmpty) Set[Array[Set[Int]]]()
+    else {
+      var matches = Set[Array[Set[Int]]]()
+      for (i <- phi(depth) if (!phi.slice(0, depth).flatten.contains(i))) {
+        val phiCopy = phi.map(x=>x.map(y=>y))
+        phiCopy(depth) = Set[Int](i)
+        matches = matches ++ saltzIso(g, q, saltzDualSim(g, q, phiCopy), depth + 1)
+      }
+      matches
+    }
+  }
+
   def saltzDualSim(g: Graph, q: Graph): Array[Set[Int]] = {
-    val phi = feasibleMates(g, q) 
+    saltzDualSim(g, q, feasibleMates(g, q))
+  }
+
+  def saltzDualSim(g: Graph, q: Graph, phi: Array[Set[Int]]): Array[Set[Int]] = {
+    //val phi = feasibleMates(g, q) 
     // TODO: check if any of them are empty... Although you find that out 
     // quickly enough
     var changed = true
